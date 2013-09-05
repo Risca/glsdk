e8996840093700378478e8dd93b001a6b71a5b68
Nikhil Devshatwar <nikhil.nd@ti.com>
2013-09-05 16:09:27 +0530

    Added script for eMMC boot
    Added boot.script.emmc
    Added a script for eMMC boot in bin/ directory
    This is required to be run on target
    Modified mksdboot.sh to copy this script on rootfs
    
    Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

--- bin/mk-eMMC-boot.sh
+++ bin/mk-eMMC-boot.sh
@@ -1,13 +1,15 @@
 #! /bin/sh
-# Script to create SD card for OMAP5 uevm.
+# Script to create bootable eMMC for OMAP5/DRA7xx evm.
 #
 # Author: Brijesh Singh, Texas Instruments Inc.
-#       : Adapted for omap5-uevm by Nikhil Devshatwar, Texas Instruments Inc.
+#       : Adapted for dra7xx-evm by Nikhil Devshatwar, Texas Instruments Inc.
+#       : Adapted for bootable eMMC by Alaganraj, Texas Instruments Inc.
 #
 # Licensed under terms of GPLv2
 #
 
 VERSION="0.1"
+mmc_dev="/dev/mmcblk0"
 
 execute ()
 {
@@ -24,7 +26,7 @@ version ()
 {
   echo
   echo "`basename $1` version $VERSION"
-  echo "Script to create bootable SD card for omap5-uevm"
+  echo "Script to create bootable eMMC for omap5/dra7xx evm"
   echo
 
   exit 0
@@ -36,8 +38,7 @@ usage ()
 Usage: `basename $1` <options> [ files for install partition ]
 
 Mandatory options:
-  --device              SD block device node (e.g /dev/sdd)
-  --sdk                 Where is sdk installed ?
+  --device              eMMC block device node (e.g /dev/mmcblk1)
 
 Optional options:
   --version             Print version.
@@ -61,6 +62,11 @@ check_if_main_drive ()
 
 }
 
+
+echo "This has to be run on target, don't run on host"
+echo "Are you running on target? (Press ENTER to continue)"
+read junkdata
+
 # Check if the script was started as root or with sudo
 user=`id -u`
 [ "$user" != "0" ] && echo "++ Must be root/sudo ++" && exit
@@ -72,19 +78,19 @@ while [ $# -gt 0 ]; do
       usage $0
       ;;
     --device) shift; device=$1; shift; ;;
-    --sdk) shift; sdkdir=$1; shift; ;;
+#    --sdk) shift; sdkdir=$1; shift; ;;
     --version) version $0;;
     *) copy="$copy $1"; shift; ;;
   esac
 done
 
-test -z $sdkdir && usage $0
+#test -z $sdkdir && usage $0
 test -z $device && usage $0
 
-if [ ! -d $sdkdir ]; then
-   echo "ERROR: $sdkdir does not exist"
-   exit 1;
-fi
+#if [ ! -d $sdkdir ]; then
+#   echo "ERROR: $sdkdir does not exist"
+#   exit 1;
+#fi
 
 if [ ! -b $device ]; then
    echo "ERROR: $device is not a block device file"
@@ -105,7 +111,7 @@ echo "*         Press <ENTER> to confirm....                     *"
 echo "************************************************************"
 read junk
 
-for i in `ls -1 $device?`; do
+for i in `ls -1 ${device}p?`; do
  echo "unmounting device '$i'"
  umount $i 2>/dev/null
 done
@@ -114,12 +120,12 @@ execute "dd if=/dev/zero of=$device bs=1024 count=1024"
 
 sync
 
-cat << END | fdisk $device
+cat << END | fdisk -H 255 -S 63 $device
 n
 p
 1
 
-+62M
++64M
 n
 p
 2
@@ -145,54 +151,45 @@ if [ ! -b ${PARTITION2} ]; then
 fi
 
 # make partitions.
-echo "Formating ${device}1 ..."
+echo "Formating ${device}p1 ..."
 if [ -b ${PARTITION1} ]; then
 	mkfs.vfat -F 32 -n "boot" ${PARTITION1}
 else
 	echo "Cant find boot partition in /dev"
 fi
 
+echo "Formating ${device}p2 ..."
 if [ -b ${PARITION2} ]; then
 	mkfs.ext4 -L "rootfs" ${PARTITION2}
 else
 	echo "Cant find rootfs partition in /dev"
 fi
 
-echo "Copying filesystem on ${device}1,${device}2"
-execute "mkdir -p /tmp/sdk/$$/boot"
-execute "mkdir -p /tmp/sdk/$$/rootfs"
-execute "mount ${device}1 /tmp/sdk/$$/boot"
-execute "mount ${device}2 /tmp/sdk/$$/rootfs"
+echo "Preparing for Copy..."
+execute "mkdir -p /tmp/sdk/$$/mmc_boot"
+execute "mkdir -p /tmp/sdk/$$/mmc_rootfs"
+execute "mkdir -p /tmp/sdk/$$/emmc_boot"
+execute "mkdir -p /tmp/sdk/$$/emmc_rootfs"
 
-echo "Creating boot.scr.sd from boot.script.sd"
-mkimage -A arm -T script -C none -n "Boot Image" -d "$sdkdir/board-support/prebuilt-images/boot.script.sd" "$sdkdir/board-support/prebuilt-images/boot.scr.sd"
+execute "mount ${mmc_dev}p1 /tmp/sdk/$$/mmc_boot"
+execute "mount ${mmc_dev}p2 /tmp/sdk/$$/mmc_rootfs"
+execute "mount ${device}p1 /tmp/sdk/$$/emmc_boot"
+execute "mount ${device}p2 /tmp/sdk/$$/emmc_rootfs"
 
-echo "Creating boot.scr.emmc from boot.script.emmc"
-mkimage -A arm -T script -C none -n "Boot Image" -d "$sdkdir/board-support/prebuilt-images/boot.script.emmc" "$sdkdir/board-support/prebuilt-images/boot.scr.emmc"
+echo "Copying boot image from ${mmc_dev}p1 to ${device}p1"
+execute "cp /tmp/sdk/$$/mmc_boot/* /tmp/sdk/$$/emmc_boot/."
+execute "rm /tmp/sdk/$$/emmc_boot/boot.scr"
+execute "mv /tmp/sdk/$$/emmc_boot/boot.scr.eMMC /tmp/sdk/$$/emmc_boot/boot.scr"
 
-execute "cp $sdkdir/board-support/prebuilt-images/MLO /tmp/sdk/$$/boot/"
-execute "cp $sdkdir/board-support/prebuilt-images/u-boot.img /tmp/sdk/$$/boot/"
-execute "cp $sdkdir/board-support/prebuilt-images/uImage /tmp/sdk/$$/boot/"
-execute "cp $sdkdir/board-support/prebuilt-images/omap5-uevm.dtb /tmp/sdk/$$/boot/"
-execute "cp $sdkdir/board-support/prebuilt-images/boot.scr.sd /tmp/sdk/$$/boot/boot.scr"
-execute "cp $sdkdir/board-support/prebuilt-images/boot.scr.emmc /tmp/sdk/$$/boot/boot.scr.emmc"
-
-execute "cp $sdkdir/bin/README.boot.scr /tmp/sdk/$$/boot/"
-
-if [ ! -f $sdkdir/filesystem/arago*sdk*console*omap5*.tar.gz ]
-then
-	echo "ERROR: failed to find rootfs tar in $sdkdir/filesystem"
-else
-	echo "Extracting filesystem on ${device}2 ..."
-	root_fs=`ls -1 $sdkdir/filesystem/arago*sdk*console*omap5*.tar.gz`
-	execute "tar zxf $root_fs -C /tmp/sdk/$$/rootfs"
-	execute "cp $sdkdir/bin/mk-eMMC-boot.sh /tmp/sdk/$$/rootfs/home/root/"
-fi
+echo "Copying filesystem from ${mmc_dev}p2 to ${device}p2"
+execute "cp -rvf /tmp/sdk/$$/mmc_rootfs/* /tmp/sdk/$$/emmc_rootfs/."
 
 sync
-echo "unmounting ${device}1,${device}2"
-execute "umount /tmp/sdk/$$/boot"
-execute "umount /tmp/sdk/$$/rootfs"
+echo "unmounting ${mmc_dev}p1,${mmc_dev}p2,${device}p1,${device}p2"
+execute "umount /tmp/sdk/$$/mmc_boot"
+execute "umount /tmp/sdk/$$/mmc_rootfs"
+execute "umount /tmp/sdk/$$/emmc_boot"
+execute "umount /tmp/sdk/$$/emmc_rootfs"
 
 execute "rm -rf /tmp/sdk/$$"
 echo "completed!"
