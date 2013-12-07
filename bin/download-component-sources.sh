da8c1b115fd0bceae87deed46157807e1213a239
Pradeep Venkatasubbarao <pradeepv@ti.com>
2013-12-07 16:01:28 +0530

    [IPUMM] Updated tool versions
    XDC: 3.25.04.88
    BIOS:6.35.03.47
    
    Removed OSAL from download.
    
    Signed-off-by: Pradeep Venkatasubbarao <pradeepv@ti.com>

--- bin/download-component-sources.sh
+++ bin/download-component-sources.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 #Component Sources versions:
-FRAMEWORK_COMP_VERSION="3_24_00_09"
+FRAMEWORK_COMP_VERSION="3_24_02_15"
 CODEC_ENGINE_VERSION="3_24_00_08"
-OSAL_VERSION="1_24_00_09"
+#OSAL_VERSION="1_24_00_09"
 XDAIS_VERSION="7_24_00_04"
-BIOS_VERSION="6_35_02_45"
-XDCTOOLS_VERSION="3_25_02_70"
+BIOS_VERSION="6_35_03_47"
+XDCTOOLS_VERSION="3_25_04_88"
 
 FRAMEWORK_COMP_WGET_URL="http://downloads.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/fc/$FRAMEWORK_COMP_VERSION/exports/framework_components_$FRAMEWORK_COMP_VERSION.tar.gz"
 CODEC_ENGINE_WGET_URL="http://downloads.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/ce/$CODEC_ENGINE_VERSION/exports/codec_engine_$CODEC_ENGINE_VERSION.tar.gz"
-OSAL_WGET_URL="http://downloads.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/osal/$OSAL_VERSION/exports/osal_$OSAL_VERSION.tar.gz"
+#OSAL_WGET_URL="http://downloads.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/osal/$OSAL_VERSION/exports/osal_$OSAL_VERSION.tar.gz"
 XDAIS_WGET_URL="http://downloads.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/xdais/$XDAIS_VERSION/exports/xdais_$XDAIS_VERSION.tar.gz"
 BIOS_WGET_URL="http://downloads.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/bios/sysbios/$BIOS_VERSION/exports/bios_setuplinux_$BIOS_VERSION.bin"
 XDCTOOLS_WGET_URL="http://downloads.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/rtsc/$XDCTOOLS_VERSION/exports/xdctools_setuplinux_$XDCTOOLS_VERSION.bin"
@@ -30,12 +30,12 @@ if [ ! -d "component-sources/codec_engine_$CODEC_ENGINE_VERSION" ]; then
 	tar -zxf codec_engine_$CODEC_ENGINE_VERSION.tar.gz -C component-sources/
 	mv codec_engine*.tar.gz component-sources/
 fi
-if [ ! -d "component-sources/osal_$OSAL_VERSION" ]; then
-	wget -nc $OSAL_WGET_URL
-	echo "Extracting OSAL..."
-	tar -zxf osal_$OSAL_VERSION.tar.gz -C component-sources/
-	mv osal*.tar.gz component-sources/
-fi
+#if [ ! -d "component-sources/osal_$OSAL_VERSION" ]; then
+#	wget -nc $OSAL_WGET_URL
+#	echo "Extracting OSAL..."
+#	tar -zxf osal_$OSAL_VERSION.tar.gz -C component-sources/
+#	mv osal*.tar.gz component-sources/
+#fi
 if [ ! -d "component-sources/xdais_$XDAIS_VERSION" ]; then
 	wget -nc $XDAIS_WGET_URL
 	echo "Extracting XDAIS..."
