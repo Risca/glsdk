bf127098b586448a058faa29d061b359ab61dd6d
Karthik Ramanan <a0393906@ti.com>
2014-04-14 14:23:28 +0530

    omap5: Updated setup-yocto script
    Signed-off-by: Karthik Ramanan <a0393906@ti.com>

--- bin/setup-yocto.sh
+++ bin/setup-yocto.sh
@@ -5,9 +5,11 @@ echo "[GLSDK]> Testing for "yocto-layers""
 if [ -d "yocto-layers" ]; then
 	echo "[GLSDK]> yocto-layers is present. Proceed to next step"
 else 
-	echo "[GLSDK]> yocto-layers is not present. Cloning it."
-	echo "[GLSDK]> git clone git://arago-project.org/git/projects/oe-layersetup.git yocto-layers"
-	git clone git://arago-project.org/git/projects/oe-layersetup.git yocto-layers
+	echo "[GLSDK]> ERROR: yocto-layers is not present"
+	echo "[GLSDK]>        Please run setup.sh and answer 'yes' to fetch-sources prompt"
+	echo "[GLSDK]>        Refer to the GLSDK Software Developers Guide for details"
+	echo "[GLSDK]> Exiting..."
+	exit
 fi
 
 echo "[GLSDK]> Copying yocto build-scripts to yocto-layers"
@@ -20,9 +22,6 @@ cd yocto-layers
 
 echo ["GLSDK]>  git am ../patches/*.patch "
 git am ../patches/*.patch 
-
-echo "[GLSDK]> ./oe-layertool-setup.sh -f configs/glsdk/glsdk-06.04.00.02-config.txt"
-./oe-layertool-setup.sh -f configs/glsdk/glsdk-06.04.00.02-config.txt
 
 echo "[GLSDK]>"
 echo "[GLSDK]> Completed Yocto setup. You can now build using Yocto"
