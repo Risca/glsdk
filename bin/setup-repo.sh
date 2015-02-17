d13ee53985b147734803579c69845402fdc6e322
Karthik Ramanan <a0393906@ti.com>
2015-02-17 12:52:08 +0530

    glsdk: Added first adaptation of scripts for 7.10.00.04 release
    Signed-off-by: Karthik Ramanan <a0393906@ti.com>

--- bin/setup-repo.sh
+++ bin/setup-repo.sh
@@ -21,7 +21,7 @@ fetch_repo() {
     echo "Successfully extracted the repo tool to $cwd"
     echo "Proceeding with repo init -u git://git.ti.com/glsdk/release-manifest.git"
     echo " "
-    repo init -u git://git.ti.com/glsdk/release-manifest.git -m dra7xx-evm_6_10_00_02.xml --no-repo-verify
+    repo init -u git://git.ti.com/glsdk/release-manifest.git -m dra7xx-evm_7_00_00_04.xml --no-repo-verify
     check_status
     echo
 }
