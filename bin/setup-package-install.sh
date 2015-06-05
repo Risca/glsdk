e4d7ed82ebb838ee735b1a11a1888d8b3a18a741
Venkateswara Rao Mandela <venkat.mandela@ti.com>
2015-06-05 16:40:52 +0530

    Add lzop to the set of installed packages
    This package is required to use lzo as the compresssion format for
    kernel image instead of gzip.  lzo compression provides faster
    decompression speed on the target compared to gzip while keeping the
    size of the compressed binary roughly the same.
    
    During my benchmarking, gzip yielded kernel of size 4.4 MB and
    decompresssion time of 196 ms. lzo compression yielded kernel size of
    4.6 MB and decompressions time of 69 ms.
    
    Signed-off-by: Venkateswara Rao Mandela <venkat.mandela@ti.com>
    Signed-off-by: Karthik Ramanan <a0393906@ti.com>

--- bin/setup-package-install.sh
+++ bin/setup-package-install.sh
@@ -2,7 +2,7 @@
 
 . `dirname $0`/common.sh
 
-cmd="sudo apt-get install xinetd tftpd nfs-kernel-server minicom build-essential libncurses5-dev uboot-mkimage autoconf automake python git diffstat texinfo gawk chrpath subversion dos2unix m2crypto"
+cmd="sudo apt-get install xinetd tftpd nfs-kernel-server minicom build-essential libncurses5-dev uboot-mkimage autoconf automake python git diffstat texinfo gawk chrpath subversion dos2unix m2crypto lzop"
 echo "--------------------------------------------------------------------------------"
 echo "This step will make sure you have the proper host support packages installed"
 echo "using the following command: $cmd"
