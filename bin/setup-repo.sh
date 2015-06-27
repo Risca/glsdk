c5714bc3fe6800a2b94280639083dc460ec3c66b
Karthik Ramanan <a0393906@ti.com>
2015-06-27 08:51:55 +0530

    setup-repo: Point to the correct manifest
    Signed-off-by: Karthik Ramanan <a0393906@ti.com>

--- bin/setup-repo.sh
+++ bin/setup-repo.sh
@@ -21,7 +21,7 @@ fetch_repo() {
     echo "Successfully extracted the repo tool to $cwd"
     echo "Proceeding with repo init -u git://git.ti.com/glsdk/release-manifest.git"
     echo " "
-    repo init -u git://git.ti.com/glsdk/release-manifest.git -m dra7xx-evm_7_00_00_04.xml --no-repo-verify
+    repo init -u git://git.ti.com/glsdk/release-manifest.git -m dra7xx-evm_7_01_00_03.xml --no-repo-verify
     check_status
     echo
 }
