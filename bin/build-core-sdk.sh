06e49da0dbf02dc9af264b9da932de9b14e11428
Karthik Ramanan <a0393906@ti.com>
2016-07-20 16:36:44 +0530

    update Yocto build script for 03.00.00.03 release
    Signed-off-by: Karthik Ramanan <a0393906@ti.com>

--- bin/build-core-sdk.sh
+++ bin/build-core-sdk.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+TOOLS_DOWNLOAD_LOCATION=$(HOME)
+
+GCC_LINARO_TOOLCHAIN_PATH=$TOOLS_DOWNLOAD_LOCATION/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/bin/
+CCS_DOWNLOAD_LOCATION=$TOOLS_DOWNLOAD_LOCATION
+CCS_VERSION=CCS6.1.3.00033_linux.tar.gz
+
+if [ -d "$GCC_LINARO_TOOLCHAIN_PATH" ]; then
+    echo "SUCCESS: GCC Linaro tool chain path has been set correctly"
+else
+    echo "ERROR: GCC Linaro tool chain does not exist at $GCC_LINARO_TOOLCHAIN_PATH. Please download or set the path correct."
+    exit 1
+fi
+
+
+if [ -f "$CCS_DOWNLOAD_LOCATION/$CCS_VERSION" ]; then
+    echo "SUCCESS: CCS path has been set correctly"
+else
+    echo "ERROR: CCS has not been downloaded, please download it from TI website and place it in this path: $CCS_DOWNLOAD_LOCATION/$CCS_VERSION"
+    exit 1
+fi 
+
 dwndefault="${PSDKLA}/yocto-layers/downloads"
 
 echo "In which directory do you want to place the downloads for the Yocto build ?(if this directory does not exist it will be created)"
@@ -35,8 +56,11 @@ cp conf/local.conf conf/local.conf.pristine
 mkdir -p $dwn
 sed -i -e "s#^DL_DIR =.*#DL_DIR = \"${dwn}\"#" conf/local.conf
 
-echo "[PSDKLA]> MACHINE=$1 bitbake arago-glsdk-multimedia-image"
-MACHINE=$1 bitbake arago-glsdk-multimedia-image
+cp $CCS_DOWNLOAD_LOCATION/$CCS_VERSION $dwn/.
+touch $dwn/$CCS_VERSION.done
+
+echo "[PSDKLA]> MACHINE=$1 bitbake tisdk-rootfs-image "
+MACHINE=$1 bitbake tisdk-rootfs-image 
 
 echo "[PSDKLA]>"
 echo "[PSDKLA]> Completed Yocto build at `date`"
